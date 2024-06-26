382968d01a476867cfff52579bc4dd1b93a8e02b
SarvKaur507 <kaursarvjeet507@gmail.com>
2024-06-26 16:04:32 +0530

    add crential

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -29,6 +29,9 @@ namespace Forum.Controllers
         public IActionResult Registration(User user)
         {
             bool Data = _home.Adduser(user);
+
+
+
             return RedirectToAction("Home/Index");
         }
     }
